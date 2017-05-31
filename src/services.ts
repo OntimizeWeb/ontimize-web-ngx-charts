c3c0c5c12a78b57ae84f0849eb87a5d5f98bcf35
Gonzalo Martínez <gonzalo.martinez@imatia.com>
2017-05-31 12:39:20 +0200

    Updating providers export functions

--- src/services.ts
+++ src/services.ts
@@ -2,11 +2,15 @@ import { Injector } from '@angular/core';
 import { MdIconRegistry } from '@angular/material';
 import { ChartService } from './services/chart.service';
 
+export function getChartServiceProvider() {
+    return new ChartService();
+}
+
 export const OCHART_PROVIDERS: any = [
   MdIconRegistry,
   {
     provide: ChartService,
-    useFactory: () => new ChartService(),
+    useFactory: getChartServiceProvider,
     deps: [Injector]
   }
 ];
