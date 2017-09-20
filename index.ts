966db08a5a0e2bc076a1c5097bd6e8bf15bc3070
gonzalo.martinez <gonzalo.martinez@imatia.com>
2017-09-20 11:59:14 +0200

    Updating index

--- index.ts
+++ index.ts
@@ -1,9 +1,4 @@
 import { NgModule } from '@angular/core';
-// import {
-//   NgModule,
-//   CUSTOM_ELEMENTS_SCHEMA
-// } from '@angular/core';
-
 import { OCHART_MODULES } from './src/components';
 import { OCHART_PROVIDERS } from './src/services';
 
@@ -15,17 +10,10 @@ export * from './src/core';
 export * from './src/interfaces';
 export * from './src/services';
 
-// import 'd3';
-// import 'nvd3';
-
 @NgModule({
   imports: OCHART_MODULES,
   exports: OCHART_MODULES,
   providers: OCHART_PROVIDERS
-  // ,
-  // schemas: [
-  //   CUSTOM_ELEMENTS_SCHEMA
-  // ]
 })
 export class OChartModule {
 }
