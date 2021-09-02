036feaa38312c0efd97f0adc37bbbac634d64d6c
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2021-09-02 19:26:53 +0200

    Updated version and ontimize-web-ngx dependencies

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   versions: {
-    core: '8.3.0',
+    core: '8.5.0',
     charts: '8.0.0',
-    theming: '8.0.1'
+    theming: '8.1.0'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@
 export const environment = {
   production: false,
   versions: {
-    core: '8.3.0',
+    core: '8.5.0',
     charts: '8.0.0',
-    theming: '8.0.1'
+    theming: '8.1.0'
   }
 };
