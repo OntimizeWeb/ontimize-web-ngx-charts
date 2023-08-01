aed1c07c7033deaedc2a7217eed9ffa50891446e
tomfacal <88186642+tomfacal@users.noreply.github.com>
2023-08-01 15:21:39 +0200

    added multi bar chart

--- projects/ontimize-web-ngx-charts/src/lib/adapters/multibar-data-adapter.ts
+++ projects/ontimize-web-ngx-charts/src/lib/adapters/multibar-data-adapter.ts
@@ -1,12 +1,18 @@
-import { ChartSeries } from '../interfaces/ChartData.interface';
 import { ChartDataAdapter } from '../interfaces/ChartDataAdapterFactory.interface';
 import { ChartConfiguration } from '../models/ChartConfiguration.class';
 import { MultiBarChartConfiguration } from '../models/options/MultiBarChartConfiguration.class';
+interface ChartDataPoint {
+  name: string;
+  value: number;
+}
 
+interface ChartSeries {
+  name: string;
+  series: ChartDataPoint[];
+}
 export class MultiBarDataAdapter implements ChartDataAdapter {
 
   protected chartConf: ChartConfiguration;
-
   protected xAxis: string;
   protected yAxis: string[];
 
@@ -23,23 +29,32 @@ export class MultiBarDataAdapter implements ChartDataAdapter {
     const result: ChartSeries[] = [];
     const params = this.chartConf as MultiBarChartConfiguration;
     if (data && data.length) {
-      const seriesvalues = this.processSeriesValues(data);
+      const seriesValues = this.processSeriesValues(data);
       const self = this;
       this.yAxis.forEach((axis: string, _index: number) => {
+        const seriesDataPoints: ChartDataPoint[] = [];
+        seriesValues[axis].forEach((item: any, _pointIndex: number) => {
+          const dataPoint: ChartDataPoint = {
+            name: item.x,
+            value: item.y
+          };
+          seriesDataPoints.push(dataPoint);
+        });
+
         const serie: ChartSeries = {
-          key: 'series',
-          values: []
+          name: axis,
+          series: seriesDataPoints
         };
+
         if (params.color && params.color[_index]) {
-          serie.color = params.color[_index];
+          serie['color'] = params.color[_index];
         }
+
         let key = axis;
         if (self.chartConf.translateService) {
           key = self.chartConf.translateService.get(key);
         }
-        serie.key = key;
-        seriesvalues[axis].sort((a, b) => (a.x > b.x) ? 1 : (b.x > a.x) ? -1 : 0);
-        serie.values = seriesvalues[axis];
+        serie.name = key;
 
         result.push(serie);
       });
@@ -48,23 +63,20 @@ export class MultiBarDataAdapter implements ChartDataAdapter {
   }
 
   public processSeriesValues(data: Object[]): Object {
-    const seriesvalues = {};
+    const seriesValues = {};
     const self = this;
     data.forEach((item: any, _index: number) => {
-
       self.yAxis.forEach((axis: string, _axisIndex: number) => {
-        if (seriesvalues[axis] === undefined) {
-          seriesvalues[axis] = [];
+        if (seriesValues[axis] === undefined) {
+          seriesValues[axis] = [];
         }
         const val = {
           x: item[self.xAxis],
           y: item[axis]
         };
-        seriesvalues[axis].push(val);
+        seriesValues[axis].push(val);
       });
     });
-    return seriesvalues;
+    return seriesValues;
   }
-
 }
-
