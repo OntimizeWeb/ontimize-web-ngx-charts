63e3f3435f9b804fc887e69e4f8faeb6f1af194f
tomfacal <88186642+tomfacal@users.noreply.github.com>
2023-08-16 10:19:44 +0200

    updated to angular 13

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -23,17 +23,7 @@ import 'zone.js';
 */
 
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following to support `@angular/animation`. */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-
 /** Evergreen browsers require these. **/
-
-/** ALL Firefox browsers require the following to support `@angular/animation`. **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 
