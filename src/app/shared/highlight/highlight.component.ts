b2e0f419e95f1980b15ca97e843b1f2ceda1608c
tomfacal <88186642+tomfacal@users.noreply.github.com>
2023-11-16 12:49:36 +0100

    modified styles and example codes

--- src/app/shared/highlight/highlight.component.ts
+++ src/app/shared/highlight/highlight.component.ts
@@ -1,7 +1,11 @@
 import {
+  ChangeDetectionStrategy,
   Component,
   ViewEncapsulation
 } from '@angular/core';
+import { HighlightLoader } from 'ngx-highlightjs';
+import { AppearanceService } from 'ontimize-web-ngx';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'highlight-comp',
@@ -11,13 +15,37 @@ import {
     'templateContent: template-content',
     'templateType: template-type'
   ],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HighlightComponent {
 
-  constructor() {
+  templateContent: any;
+  templateType: any;
+  templateTypeArray: Array<string>;
+  private onThemeUpdate: Subscription;
+
+  constructor(
+    private hljsLoader: HighlightLoader,
+    private appearanceService: AppearanceService
+  ) {
+    this.loadTheme(this.appearanceService.isDarkMode());
+    this.onThemeUpdate = this.appearanceService.isDarkMode$.subscribe((darkMode: boolean) => {
+      this.loadTheme(darkMode);
+    });
   }
 
+  loadTheme(darkMode: boolean) {
+    this.hljsLoader.setTheme(darkMode ? 'assets/hightlight/github-dark.css' : 'assets/hightlight/github.css');
+  }
+
+  ngOnInit(): void {
+    this.templateTypeArray = this.parseTemplateType();
+  }
+
+  parseTemplateType() {
+    return [this.templateType === 'scss' ? 'css' : this.templateType];
+  }
 
   showTooltip() {
     // TODO show tooltip (solve problem of repainting that does not show tooltip)
