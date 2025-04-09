2789436e3baa09115329f117727460e6653ff726
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2025-04-09 10:46:15 +0200

    Removed ChartTranslateService and merge i18n in translations OTranslateService

--- projects/ontimize-web-ngx-charts/src/lib/components/chart-on-demand/o-chart-on-demand.module.ts
+++ projects/ontimize-web-ngx-charts/src/lib/components/chart-on-demand/o-chart-on-demand.module.ts
@@ -4,15 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { O_CHART_ON_DEMAND_SERVICE, OntimizePreferencesService, OntimizeWebModule, preferencesServiceFactory } from 'ontimize-web-ngx';
 
 import { OChartOnDemandService } from '../../services/chart-on-demand.service';
-import { OChartTranslatePipe } from '../../util/o-chart-translate.pipe';
 import { OChartComponentModule } from '../chart/o-chart.module';
 import { LoadPreferencesDialogComponent } from './load-preferences-dialog/load-preferences-dialog.component';
 import { OChartOnDemandComponent } from './o-chart-on-demand.component';
 import { SavePreferencesDialogComponent } from './save-preferences-dialog/save-preferences-dialog.component';
+import { ChartTranslateService } from '../../services/chart-translate.service';
 
 
 @NgModule({
-  declarations: [OChartOnDemandComponent, SavePreferencesDialogComponent, LoadPreferencesDialogComponent, OChartTranslatePipe],
+  declarations: [OChartOnDemandComponent, SavePreferencesDialogComponent, LoadPreferencesDialogComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
@@ -25,4 +25,8 @@ import { SavePreferencesDialogComponent } from './save-preferences-dialog/save-p
    { provide: OntimizePreferencesService, useFactory: preferencesServiceFactory, deps: [Injector] }
   ]
 })
-export class OChartOnDemandComponentModule { }
+export class OChartOnDemandComponentModule {
+  constructor(private translationService: ChartTranslateService) {
+    this.translationService.loadTranslations();
+  }
+ }
